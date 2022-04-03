f526fe73452467bedef3ec1a31f325818f576aa7
unknown <mbelhedi@outlook.com>
2022-04-03 11:59:42 +0200

    Das ist Sophias Seite noch in der RohPhase update 03.04.22 um 12:00 Uhr

--- js/functions.js
+++ js/functions.js
@@ -32,21 +32,52 @@ window.open("https://instagram.com/sophialouise_fotografie?utm_medium=copy_link"
 function whatsapp()
 {
 
-var message=confirm("Mit dem Klicken auf dem Whatapp Button erklären Sie sich damit einverstanden, das sie diesen Service der Facebook Gruppe nutzen um mit mir zu kommunizieren, wenn Sie diese nicht wünschen dann bitte auf abbrechen Klicken")
+// var message=confirm("Mit dem Klicken auf dem Whatapp Button erklären Sie sich damit einverstanden, das sie diesen Service der Facebook Gruppe nutzen um mit mir zu kommunizieren, wenn Sie diese nicht wünschen dann bitte auf abbrechen Klicken")
 var text='Hallo Meine Name......., ich möchte einen Fotoshooting'
 
-if(message)
-{
+Swal.fire({
+    title: 'Sicher das du über Whatapp mit mir Kommunzieren willst?',
+    text: "Mit dem Klicken auf dem Whatapp Button erklären Sie sich damit einverstanden, das sie diesen Service der Facebook Gruppe nutzen um mit mir zu kommunizieren, wenn Sie diese nicht wünschen dann bitte auf abbrechen Klicken",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    cancelButtonText: 'Nein',
+    confirmButtonText: 'Ja '
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Danke',
+        'Du wirst zu meinem Whatapp Chat weitergeleitet',
+        'success'
+        
+      )
+      
+    setTimeout(function(){
 
-    window.open("https://wa.me/4915757814627?text="+text,"_blank")
+        window.open("https://wa.me/4915757814627?text="+text,"_blank")
 
 
-}else
-{
 
-return false;
 
-}
+    },2300)
+
+    }
+  })
+
+
+// if(message)
+// {
+
+//     window.open("https://wa.me/4915757814627?text="+text,"_blank")
+
+
+// }else
+// {
+
+// return false;
+
+// }
 
 
 
