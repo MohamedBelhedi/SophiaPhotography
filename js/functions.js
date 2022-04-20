d4f9d8bb0268ae82dfb1de1c5bb81ee76c2945e3
unknown <mbelhedi@outlook.com>
2022-04-21 00:25:16 +0200

    update Sophias Seite am 21.04.22 um 00:23 Uhr

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,23 @@
-function bilder()
-{
-
-var bilder=document.querySelector('.bilder')
-
-
-
-
-// if(bilder)
-
+// function bilder()
 // {
 
+// var bilder=document.querySelector('.bilder')
 
-//     alert("Hallo ich Bin Sophia Nofz")
+
+
+
+// // if(bilder)
+
+// // {
+
+
+// //     alert("Hallo ich Bin Sophia Nofz")
+
+
+// // }
 
 
 // }
-
-
-}
 
 
 function insta()
@@ -81,4 +81,80 @@ Swal.fire({
 
 
 
+
+
+
 }
+
+let modal1=document.querySelector(".modal1")
+let modal2=document.querySelector(".modal2")
+let modal3=document.querySelector(".modal3")
+
+let bild1=document.getElementById("bild1")
+let bild2=document.getElementById("bild2")
+let bild3=document.getElementById("bild3")
+let bild4=document.getElementById("bild4")
+
+let close1=document.querySelector("#close1")
+let close2=document.querySelector("#close2")
+let close3=document.querySelector("#close3")
+let close4=document.querySelector("#close4")
+
+
+function modal_open1(){modal1.showModal()}
+function modal_open2(){modal2.showModal()}
+function modal_open3(){modal3.showModal()}
+function modal_open4(){modal4.showModal()}
+
+function close_modal1(){modal1.close();console.log("zu1")}
+function close_modal2(){modal2.close();console.log("zu2")}
+function close_modal3(){modal3.close();console.log("zu3")}
+function close_modal4(){modal4.close();console.log("zu4")}
+
+function service(){
+
+bild1.addEventListener("click",modal_open1)
+bild2.addEventListener("click",modal_open2)
+bild3.addEventListener("click",modal_open3)
+bild4.addEventListener("click",modal_open4)
+
+close1.addEventListener("click",close_modal1)
+close2.addEventListener("click",close_modal2)
+close3.addEventListener("click",close_modal3)
+close4.addEventListener("click",close_modal4)
+
+
+
+
+
+  // dann hier modals öffnen lassen für service
+  // bild.forEach(()=>{
+
+  //   bild[0].addEventListener("click",()=>{modal_open1;console.log("hi1")})
+  //   bild[1].addEventListener("click",()=>{modal_open2;console.log("hi2")})
+  //   bild[2].addEventListener("click",()=>{modal_open2;console.log("hi3")})
+  //   bild[3].addEventListener("click",()=>{modal_open2;console.log("hi4")})
+    
+
+  // })
+
+  }
+
+
+
+
+// var bild=document.querySelectorAll("img")
+
+// for(var i=0; i<bild.length;i++)
+// {
+
+
+//   bild[i].addEventListener("click",service)
+//   // bild[2].addEventListener("click",service)
+//   // bild[3].addEventListener("click",service)
+
+
+
+// }
+service();
+
